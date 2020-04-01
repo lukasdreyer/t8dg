86bf5d7b04e0bc0263c96a1726c7904267ab2543
Lukas Dreyer <Lukas.Dreyer@dlr.de>
2020-04-01 05:32:45 -0700

    Added ability to chose initial condition during runtime

--- src/t8dg.c
+++ src/t8dg.c
@@ -14,12 +14,29 @@
 #include "t8dg.h"
 #include "t8dg_solver.h"
 
+#include <example/common/t8_example_common.h>
+
 double
-u_analytical (const double x[DIM3], double t)
+t8dg_scalar3d_hat_function (const double x[3], const double t)
 {
-//  return 1;
-//  return (x[0] > .25 && x[0] < .75) ? 1.0 : 0.0;
-  return 0.5 - fabs (0.5 - t8_vec_norm (x));
+  return 0.5 - (fabs (0.5 - x[0]));
+}
+
+static              t8dg_scalar_function_3d_time_fn
+t8dg_choose_initial_cond_fn (int initial_cond_arg)
+{
+  switch (initial_cond_arg) {
+  case (0):
+    return t8_scalar3d_constant_one;
+  case (1):
+    return t8dg_scalar3d_hat_function;
+  case (2):
+    return t8_scalar3d_step_function;
+  case (3):
+    return t8_scalar3d_sinx;
+  default:
+    return t8_scalar3d_constant_zero;
+  }
 }
 
 int
@@ -30,6 +47,7 @@ main (int argc, char *argv[])
   char                help[BUFSIZ];
   int                 parsed, helpme;
 
+  int                 initial_cond_arg;
   int                 level;
   int                 time_order;
   int                 number_LGL_points;
@@ -64,6 +82,8 @@ main (int argc, char *argv[])
                       "The order used for the runge Kutta timestepping (1<= order <=4). Default: 2");
   sc_options_add_double (opt, 't', "start_time", &start_time, 0.0, "The start time of the solve. Default: 0.0");
   sc_options_add_double (opt, 'T', "end_time", &end_time, 1.0, "The end time of the solve. Default: 1.0");
+  sc_options_add_int (opt, 'i', "initial_cond", &initial_cond_arg, 0, "Choose initial condition function. Default: 0\n"
+                      "\t\t0: constant function\n" "\t\t1: hat function\n" "\t\t2: step function\n" "\t\t3: sine function");
 
   parsed = sc_options_parse (t8_get_package_id (), SC_LP_ERROR, opt, argc, argv);
   if (helpme) {
@@ -73,14 +93,16 @@ main (int argc, char *argv[])
   }
   else if (parsed >= 0) {
     t8_cmesh_t          cmesh;
+    t8dg_scalar_function_3d_time_fn u_initial;
 
+    u_initial = t8dg_choose_initial_cond_fn (initial_cond_arg);
     cmesh = t8_cmesh_new_hypercube (T8_ECLASS_LINE, sc_MPI_COMM_WORLD, 0, 0, 1);
 
 #if 0
     cmesh = t8_cmesh_new_periodic_line_more_trees (sc_MPI_COMM_WORLD);
 #endif
     /* Computation */
-    t8dg_advect_solve_1D (cmesh, u_analytical, flow_velocity,
+    t8dg_advect_solve_1D (cmesh, u_initial, flow_velocity,
                           level, number_LGL_points, start_time, end_time, cfl, time_order, sc_MPI_COMM_WORLD);
 #if 0
     t8_cmesh_destroy (&cmesh);  /*t8_forest_unref takes care! */
