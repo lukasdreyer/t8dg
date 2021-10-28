196b35897a5c10158aebc7bba8421da4a6aadbbb
Johannes Holke <johannes.holke@dlr.de>
2021-10-28 13:29:11 +0200

    Add mptrac flux data class

--- src/t8dg_mptrac.h
+++ src/t8dg_mptrac.h
@@ -10,12 +10,26 @@
 #define SRC_T8DG_MPTRAC_H_
 
 #include <t8/example/mptrac/t8_mptrac_interpolate.h>
-#include "t8dg.h"
+#include <t8dg.h>
+#include <t8dg_flux_data_base.h>
 
-T8DG_EXTERN_C_BEGIN ();
+class t8dg_mptrac_flux_data : public t8dg_flux_data_base
+{
+    public:
 
-t8_mptrac_context_t* t8dg_mptrac_setup (const char *nc_filename);
+    t8dg_mptrac_flux_data (const char *nc_filename);
 
-T8DG_EXTERN_C_END ();
+    ~t8dg_mptrac_flux_data ();
+
+    void initialize (const struct t8dg_linear_advection_diffusion_problem *problem);
+
+    void start_new_time_step (const struct t8dg_linear_advection_diffusion_problem *problem);
+
+
+    const t8_mptrac_context_t *get_context() const;
+
+    protected:
+        t8_mptrac_context_t * context;
+};
 
 #endif /* SRC_T8DG_MPTRAC_H_ */
