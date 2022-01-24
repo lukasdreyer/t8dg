e897c24af1de01480dabf1f2b58ebc8dde49d17e
Dreyer, Lukas <lukas.dreyer@dlr.de>
2022-01-24 17:07:01 +0100

    add licence header to source files

--- src/t8dg_geometry.h
+++ src/t8dg_geometry.h
@@ -1,9 +1,26 @@
 /*
- * geometry.h
+ *  This file is part of t8dg.
+ *  t8dg is a discontinuous galerkin solver on adaptive meshes
+ *  that uses the adaptive meshing library t8code to distribute
+ *  the workload equally among parallel processes to achieve good scaling.
  *
- *  Created on: Apr 27, 2020
- *      Author: lukas
+ *  Copyright (C) 2020 the developers
+ *
+ *  t8dg is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  t8dg is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with t8dg.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
 
 #ifndef SRC_T8DG_GEOMETRY_H_
 #define SRC_T8DG_GEOMETRY_H_
--- src/t8dg_local_values.cxx
+++ src/t8dg_local_values.cxx
@@ -1,9 +1,26 @@
 /*
- * t8dg_local_values.c
+ *  This file is part of t8dg.
+ *  t8dg is a discontinuous galerkin solver on adaptive meshes
+ *  that uses the adaptive meshing library t8code to distribute
+ *  the workload equally among parallel processes to achieve good scaling.
  *
- *  Created on: Apr 5, 2020
- *      Author: lukas
+ *  Copyright (C) 2020 the developers
+ *
+ *  t8dg is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  t8dg is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with t8dg.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
 
 #include "t8dg.h"
 #include "t8dg_local_values.h"
